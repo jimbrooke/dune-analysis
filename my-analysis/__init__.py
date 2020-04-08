f61803bdb7bd345a896bd0c6684df5fdf66338a2
Jim <jim.brooke@cern.ch>
2020-04-08 15:47:32 +0100

    add cluster finder

--- my-analysis/__init__.py
+++ my-analysis/__init__.py
@@ -1,4 +1,5 @@
 import numpy as np
+import awkward
 
 class ClusterVars():
     def __init__(self, name, out_dir, collection='marley', mask=None, out_var=None):
@@ -35,4 +36,54 @@ class ClusterVars():
         out[has_two_hits] = start_max - start_min
 
         chunk.tree.new_variable(self.out_var, out)
+        return True
+
+
+class ClusterFinder():
+    def __init__(self, name, out_dir, mask=None, out_var=None):
+        self.name = name
+        self.out_dir = out_dir
+        self.mask = mask
+        self.branches = ['Hit_Chan', 'Hit_Size', 'Hit_Start_Tick'] #this line simplified a lot
+        if out_var:
+            self.out_var = out_var
+        else:
+            self.out_var = 'clusterNHits'
+
+    def event(self, chunk):
+
+        # get the data
+        Hit_Chan, Hit_Start_Tick = chunk.tree.arrays(['Hit_Chan', 'Hit_Start_Tick'], outputtype=tuple)
+
+        # apply a mask if required
+        if self.mask:
+            mask = chunk.tree.array(self.mask)
+            Hit_Chan = Hit_Chan[mask]
+            Hit_Start_Tick = Hit_Start_Tick[mask]
+
+        clusterNHits = []
+
+        # loop over events
+        for chans, starts in zip(Hit_Chan, Hit_Start_Tick):
+
+            # channels with hits in this event
+            print("Hit channels : ",chans)
+
+            # find the clusters
+            clusters = np.split(chans, np.where(np.diff(chans) != 1)[0]+1)
+            
+            # this will store a variable for each cluster
+            n_hits_per_clus = []
+
+            # loop over clusters
+            for i, clus in zip(range(len(clusters)),clusters):
+                print(i, clus)
+                n_hits_per_clus.append(len(clus))
+            
+            # add the list of cluster variables to the output list
+            clusterNHits.append(n_hits_per_clus)
+
+        # store the new cluster variable in the event
+        chunk.tree.new_variable(self.out_var, awkward.fromiter(clusterNHits))
+
         return True
--- plot.py
+++ plot.py
@@ -3,8 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-df = pd.read_csv('output/tbl_dataset.deltaStart--clusters.csv')
+df1 = pd.read_csv('output/tbl_dataset.clusDStart--clusDStart.csv')
+df1[np.isfinite(df1['clusDStart'])].hist(column='clusDStart', bins=10)
 
-df[np.isfinite(df['deltaStart'])].hist(column='deltaStart', bins=10)
+plt.show()
+
+df2 = pd.read_csv('output/tbl_dataset.clusNHits--clusNHits.csv')
+df2[np.isfinite(df2['clusNHits'])].hist(column='clusNHits', bins=10)
 
 plt.show()
